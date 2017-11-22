caa71f11537f7ca900fd4aa92b013d7269f4b27b
Gunnari Auvinen <gauvinen@gmail.com>
2017-11-22 18:00:24 -0500

    Add redux devtools configuration to redux store.

--- src/config/store.js
+++ src/config/store.js
@@ -2,4 +2,8 @@ import { createStore } from 'redux';
 
 import reducers from '../reducers';
 
-export default createStore(reducers);
+export default createStore(
+  reducers,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
